d3186d0b44e589663a7e9d9029a12ea8526c25a3
steveystyle <92091471+steveystyle@users.noreply.github.com>
2022-05-13 20:18:12 +0100

    --- Auto-Git Commit ---

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
  Web App for the resturant Jam Jar in Paisley. Developed with: Node.JS, Express, HTML5 Boilerplate, NeDb. Handlebars templating engine and a SASS middleware for CSS and HTML dynamic creation. JQuery for client side scripts and DOM manipulation. Fontawesome and Bootstrap5 frameworks employed for layout and styling.
 
  Site uses an MVC architechture and is tested using CI/CD with Jenkins, Mocha and Chai.
+
+ Using auto commit to handle updating version and they with be tagged with "--Auto Git Commit--" any major version change will be uploaded manually and give an appropriate comit message
--- index.js
+++ index.js
@@ -3,38 +3,34 @@ const app = express();
 
 //handlebars view engine selected to use default layouts and apply logic to views for efficiency of menu display
 const handlebars = require('express-handlebars')
-.create({defaultLayout:'main'});
-
+    .create({ defaultLayout: 'main' });
+    
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
-
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
-    res.type('text/plain');
-    res.send('Jam Jar');
+    res.render('home');
 });
 
 app.get('/about', function (req, res) {
-    res.type('text/plain');
-    res.send('About Jam Jar');
+    res.render('about');
 });
 
 
 //custom 404 - update to view later
 app.use(function (req, res) {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 // custom 500 - update to view later
 app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function () {
