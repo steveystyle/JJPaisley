62b8691d439c8cfa27fb564673a0348ce3dceaef
steveystyle <92091471+steveystyle@users.noreply.github.com>
2022-05-13 19:44:28 +0100

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -2,4 +2,35 @@ const express = require('express')
 
 const app = express();
 
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function (req, res) {
+    res.type('text/plain');
+    res.send('Jam Jar');
+});
+
+app.get('/about', function (req, res) {
+    res.type('text/plain');
+    res.send('About Jam Jar');
+});
+
+
+//custom 404 - update to view later
+app.use(function (req, res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+// custom 500 - update to view later
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Server started on http://localhost:%s; press Ctrl-C to terminate.', app.get('port'));
+});
 
