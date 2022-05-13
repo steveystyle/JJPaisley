960536fcb960ecbc0ba25fa8d5b42222de685752
steveystyle <92091471+steveystyle@users.noreply.github.com>
2022-05-13 22:50:09 +0100

    --- Auto-Git Commit ---

---
+++ public/tests/tests-unit.js
@@ -0,0 +1,20 @@
+var dayQuote = require('../lib/dayQuotes.js');
+var expect = require('chai').expect;
+
+suite('Day Quote tests', function () {
+    var dayQuotesExpected = [
+        "It's Sunday.",
+        "It's Monday.",
+        "It's Tuesday.",
+        "It's Wednesday.",
+        "It's Thursday.",
+        "It's Friday.",
+        "It's Saturday.",
+    ];
+
+    test('getDayQuote() should return a quote based on current day of the week', function () {
+        expect(typeof dayQuote.getDayQuote() === 'string');
+        expect((dayQuotesExpected).to.contain(dayQuote.getDayQuote()));
+    });
+
+});
