37e9b28b1be5bd172df7ab78a78aba46318ecc10
steveystyle <92091471+steveystyle@users.noreply.github.com>
2022-05-14 21:34:41 +0100

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const expressHandlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+
 const handlers = require('./lib/handlers');
 const weatherMiddlware = require('./lib/middleware/weather');
-const bodyParser = require('body-parser');
-//handlebars view engine selected to use default layouts and apply logic to views for efficiency of menu display
-const expressHandlebars = require('express-handlebars');
+
+const app = express();
+
 app.engine('handlebars', expressHandlebars.engine({
     defaultLayout: 'main',
     helpers: {
@@ -19,18 +20,23 @@ app.engine('handlebars', expressHandlebars.engine({
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.disable('x-powered-by');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 app.use(weatherMiddlware);
 
+app.disable('x-powered-by');
+
 app.get('/', handlers.home);
 app.get('/about', handlers.about);
+
 app.get('/newsletter-signup', handlers.newsletterSignup);
 app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
+
 app.get('/newsletter', handlers.newsletter);
 app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
 
@@ -38,9 +44,9 @@ app.use(handlers.notFound);
 app.use(handlers.serverError);
 
 if (require.main === module) {
-    app.listen(port, () =>
-        console.log('Server started on http://localhost:%s; press Ctrl-C to terminate.', port)
-    );
+    app.listen(port, () => {
+        console.log('Server started on http://localhost:%s; press Ctrl-C to terminate.', port);
+    });
 } else {
     module.exports = app;
 }
