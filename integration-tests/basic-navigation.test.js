c8d8e879aa5a8306849599a89d12a10b541ea421
steveystyle <92091471+steveystyle@users.noreply.github.com>
2022-05-14 23:53:02 +0100

    --- Auto-Git Commit ---

--- integration-tests/basic-navigation.test.js
+++ integration-tests/basic-navigation.test.js
@@ -16,8 +16,13 @@ afterEach(() => {
 });
 
 test('home page links to about page', async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(`http://localhost:${port}`, {waitUntil: 'load'});
+  await page.goto(`http://localhost:${port}`, { delay: 1000 });
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('[data-test-id="about"]'),
+  ]);
+  expect(page.url()).toBe(`http://localhost:${port}/about`);
   await browser.close();
 });
