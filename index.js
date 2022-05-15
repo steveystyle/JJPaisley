b2d519238be0d7e22628c779536c326fcf77ee6d
steveystyle <92091471+steveystyle@users.noreply.github.com>
2022-05-15 20:29:55 +0100

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -43,11 +43,25 @@ app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
 app.get('/newsletter', handlers.newsletter);
 app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
 
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest);
+app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax);
+
 app.post('/contest/vacation-photo/:year/:month', (req, res) => {
     const form = new multiparty.Form();
     form.parse(req, (err, fields, files) => {
-        if (err) return res.status(500).send({ error: err.message });
+        if (err) return handlers.vacationPhotoContestProcessError(req, res, err.message);
+        console.log('got fields: ', fields);
+        console.log('and files: ', files);
         handlers.vacationPhotoContestProcess(req, res, fields, files);
+    });
+});
+app.get('/contest/vacation-photo-thank-you', handlers.vacationPhotoContestProcessThankYou);
+
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+    const form = new multiparty.Form();
+    form.parse(req, (err, fields, files) => {
+        if (err) return handlers.api.vacationPhotoContestError(req, res, err.message);
+        handlers.api.vacationPhotoContest(req, res, fields, files);
     });
 });
 
