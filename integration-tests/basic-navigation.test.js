0ee1f980c856c6cd51227b97fa6fbeeb1ac2558c
steveystyle <92091471+steveystyle@users.noreply.github.com>
2022-05-15 01:03:37 +0100

    --- Auto-Git Commit ---

--- integration-tests/basic-navigation.test.js
+++ integration-tests/basic-navigation.test.js
@@ -18,17 +18,15 @@ afterEach(() => {
 test('home page links to about page', async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setRequestInterception(true);
-  page.on('request', (request) =>{
-    request.url().endsWith('.png').abort();
-  });
-    
-  
   await page.goto(`http://localhost:${port}`);
+  await page.waitForSelector('a');
+
+  const aresult = await page.$eval('.test2');
+
+  console.log(aresult);
 
 
 
-
- // expect(page.url()).toBe(`http://localhost:${port}/about`);
+  // expect(page.url()).toBe(`http://localhost:${port}/about`);
   await browser.close();
 });
