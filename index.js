347ed8ae9f8ef15370c264603c2c48686ca9b667
steveystyle <92091471+steveystyle@users.noreply.github.com>
2022-05-15 21:54:36 +0100

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
 const multiparty = require('multiparty');
 const port = process.env.PORT || 3000;
+const { credentials } = require('./config');
 const app = express();
 
 const handlers = require('./lib/handlers');
@@ -23,6 +25,12 @@ app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser(credentials.cookieSecret));
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret,
+}));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -33,24 +41,24 @@ app.disable('x-powered-by');
 app.get('/', handlers.home);
 app.get('/about', handlers.about);
 
-app.get('/newsletter-signup', handlers.newsletterSignup);
-app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
-app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
+app.get('/newsletter-signup', handlers.newsletterSignup.Home);
+app.post('/newsletter-signup/process', handlers.newsletterSignup.Process);
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignup.ThankYou);
 
 app.get('/newsletter', handlers.newsletter);
 app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
 
-app.get('/contest/vacation-photo', handlers.vacationPhotoContest);
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest.Home);
 app.post('/contest/vacation-photo/:year/:month', (req, res) => {
     const form = new multiparty.Form();
     form.parse(req, (err, fields, files) => {
-        if (err) return handlers.vacationPhotoContestProcessError(req, res, err.message);
+        if (err) return handlers.vacationPhotoContest.ProcessError(req, res, err.message);
         console.log('got fields: ', fields);
         console.log('and files: ', files);
-        handlers.vacationPhotoContestProcess(req, res, fields, files);
+        handlers.vacationPhotoContest.Process(req, res, fields, files);
     });
 });
-app.get('/contest/vacation-photo-thank-you', handlers.vacationPhotoContestProcessThankYou);
+app.get('/contest/vacation-photo-thank-you', handlers.vacationPhotoContest.ProcessThankYou);
 
 app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax);
 app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
